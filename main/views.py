5e956f2b1c09b9128baeaa3b9c12c6e5db28702a
Alikhan3000 <fixator2004@gmail.com>
2021-06-25 23:00:01 +0600

    netr

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ def homepage(request):
     return render(request, "index.html")
 
 def test(request):
-    return render(request, "test.html.html")
+    return render(request, "test.html.")
 
 def second(request): 
     return HttpResponse("test 2 page") 
