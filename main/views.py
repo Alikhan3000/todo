f98af05796ca7a53f14361021bd8dd9557198063
Alikhan3000 <fixator2004@gmail.com>
2021-06-25 23:04:45 +0600

    hhhhhhhhhhhhhhhhh

--- main/views.py
+++ main/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render, HttpResponse
 def homepage(request):
     return render(request, "index.html")
 
-def test(request):
-    return render(request, "test.html.")
+# def test(request):
+#     return render(request, "test.html")
 
-def second(request): 
-    return HttpResponse("test 2 page") 
+# def second(request): 
+#     return HttpResponse("test 2 page") 
