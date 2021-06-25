19b55ef1fd021b2683d155b8b9fe74a2b5ceb4f5
Alikhan3000 <fixator2004@gmail.com>
2021-06-25 23:06:28 +0600

    lllllll

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
 
  #Create your views here
-def homepage(request):
-    return render(request, "index.html")
+# def homepage(request):
+#     return render(request, "index.html")
 
-# def test(request):
-#     return render(request, "test.html")
+def test(request):
+    return render(request, "index.html"))
 
 # def second(request): 
 #     return HttpResponse("test 2 page") 
+
+# def test(request):
+#     return render(request, "test.html")
