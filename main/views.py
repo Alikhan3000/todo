b2f1abf0245931da442ae1d4ab8a4672bf11df3e
Alikhan3000 <fixator2004@gmail.com>
2021-06-26 00:15:52 +0600

    media urls and roots

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
  #Create your views here
-# def homepage(request):
-#     return render(request, "index.html")
+def homepage(request):
+    return render(request, "index.html")
 
 def test(request):
     return render(request, "index.html")
