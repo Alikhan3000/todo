80201a4f8700d1ef67d674477b58517404b14a13
Alikhan3000 <fixator2004@gmail.com>
2021-07-10 20:29:31 +0600

    added text

--- main/views.py
+++ main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo
 
  #Create your views here
@@ -14,3 +14,12 @@ def test(request):
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
+
+
+def add_todo(request):
+    form = request.POST
+    text = form['todo_text']
+    todo = ToDo(text=text)
+    todo.save()
+    return redirect(test) 
+
